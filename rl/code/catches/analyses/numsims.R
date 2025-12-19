f7c8105c0a1336858f36966d771781ddde3e54d3
jnomhgn <173506318+jnomhgn@users.noreply.github.com>
2025-12-19 15:35:14 +0100

    Add C numsims for v2.

--- rl/code/catches/analyses/numsims.R
+++ rl/code/catches/analyses/numsims.R
@@ -13,7 +13,7 @@ resultsdir = "rl/results/catches/numsims"
 #### Prepare simulations ####
 
 # Number of simulated experiments
-nsim = 2
+nsim = 100
 
 # Experimental parameters (identical for all simulations)
 exp.pars = list(
@@ -446,3 +446,231 @@ p = ggarrange(p1, p2, ncol = 2, common.legend = T, legend = "right",
               labels = c("a", "b"), font.label = list(size=rel(30)))
 ggexport(p, width = 2800, height = 1440, 
          filename = paste(resultsdir, "switchtimediff_v1.jpeg", sep = "/"))
+
+
+#### Run Simulations For v2 ####
+
+# Add caching
+if(!file.exists(paste(resultsdir, "numsims_v2.rds", sep = "/"))){
+
+  # Get model list
+  models = getmodels()
+
+  # Select models 
+  models=lapply(models,function(x) x[models$name %in% c(
+                                                      "vsn2.fixed", # Winning model from NC condition
+                                                      "vsndbr2.fixed", "vsnvsr1.fixed" # Models building on winning model
+                                                      )])
+
+  # Load VSN2 model fit for the no catches condition
+  fit = readRDS(paste(resultsdir, "../../nocatches/modelcomp/adaptive/vsn2.hierarch.fit.rds", sep = "/"))
+
+  # Get parameter estimates
+  draws = fit %>%
+    tidy_draws() %>%
+    select(names(models$free.pars.pop[[which(models$name %in% c("vsn2.fixed"))]]))
+  srl.pars.nocatches = apply(draws, 2, mean)
+
+  # Extract columns with environment-specific social learning weights and put in matrix.
+
+  # Extract columns into matrix with corresponding indices
+  par.names <- grep("^alpha[^\\[]*\\[", names(srl.pars.nocatches), value = TRUE)
+
+  # Get number of different parameters that vary by environment
+  par.num = length(par.names) / 12
+
+  # Loop through parameters that vary by environment
+  for (p in 1:par.num) {
+    par.names.subset = par.names[((p - 1) * 12 + 1):(p * 12)]
+    par.indx <- do.call(rbind, regmatches(par.names.subset, gregexpr("\\d+", par.names.subset)))
+    par.mat <- matrix(NA, nrow = max(as.integer(par.indx[,1])), ncol = max(as.integer(par.indx[,2])))
+
+    for (i in seq_along(par.names.subset)) {
+      row <- as.integer(par.indx[i, 1])
+      col <- as.integer(par.indx[i, 2])
+      par.mat[row, col] <- srl.pars.nocatches[par.names.subset[i]][[1]]
+    }
+
+    # Remove alphaVSD entries from rl.pars and rename matrix
+    srl.pars.nocatches = srl.pars.nocatches[!names(srl.pars.nocatches) %in% par.names.subset]
+    par.name = unique(sub("\\[.*$", "", par.names.subset))
+
+    # Add matrix to rlpars
+    srl.pars.nocatches[par.name] = list(par.mat)
+  }
+
+  # Add fixed parameters
+  srl.pars.nocatches = append(srl.pars.nocatches, models$fixed.par[[which(models$name %in% c("vsn2.fixed"))]])
+
+  # Remove fit
+  remove(fit)
+
+  log.txt = paste(resultsdir, "log_v2.txt", sep = "/")
+  if(!file.exists(log.txt)){file.create(log.txt)}
+  
+  sink(log.txt, append = T)
+  
+  # Results list
+  results = list(
+    rew.acc = list(),
+    acc.time = list(),
+    switches = list(), switches.time = list()
+  )
+  
+  for(mod in 1:length(models$name)){
+
+    # Create rl pars
+    rl.pars.catches = list()
+
+    # Define parameter grid
+    if(models$name[[mod]] == "vsn2.fixed"){
+      rl.pars.catches = as_tibble(lapply(srl.pars.nocatches[-which(names(srl.pars.nocatches) %in% c("alphaVSD"))], function(x) x[1])) %>%
+        dplyr::mutate(alphaVSD = list(srl.pars.nocatches$alphaVSD))
+      rl.pars.catches[c("alphaDBR", "alphaVSR", "alphaVSDR", "sigmaVSDR")] = NA
+    }else if(models$name[[mod]] == "vsndbr2.fixed"){
+      alphaDBR.seq = seq(0, 1, by = .01)
+      vsndbr.pars.catches = tibble(sapply(alphaDBR.seq, function(x) list(matrix(x, nrow = length(unique(max)), ncol = length(unique(ratio)))))) %>% `colnames<-`("alphaDBR")
+      rl.pars.catches = as_tibble(lapply(srl.pars.nocatches[-which(names(srl.pars.nocatches) %in% c("alphaVSD"))], function(x) x[1])) %>%
+        dplyr::mutate(alphaVSD = list(srl.pars.nocatches$alphaVSD)) %>%
+        bind_cols(vsndbr.pars.catches)
+      rl.pars.catches[c("alphaVSR", "alphaVSDR", "sigmaVSDR")] = NA
+    }else{
+      vsnvsr.pars.catches = expand.grid(alphaVSDR = seq(0, 1, by = .01), sigmaVSDR = seq(0, 1, by = .01))
+      rl.pars.catches = as_tibble(lapply(srl.pars.nocatches[-which(names(srl.pars.nocatches) %in% c("alphaVSD"))], function(x) x[1])) %>%
+        bind_cols(vsnvsr.pars.catches)
+      rl.pars.catches[c("alphaDBR", "alphaVSR", "alphaVSD")] = NA
+    }
+    
+    # Get function
+    f = get(models$sim[[mod]])
+
+    for(parcomb in 1:nrow(rl.pars.catches)){
+      
+      # Get rl.pars and add to sim pars
+      sim.pars = c(exp.pars, env.pars, sapply(rl.pars.catches[parcomb, ], function(x) x))
+
+      # Run simulations in parallel
+      plan(multisession, workers = parallel::detectCores()-1)
+      sim_results = future.apply::future_lapply(
+        1:nsim,
+        sim_fun,
+        sim.pars = sim.pars,
+        rl.pars = rl.pars.catches,
+        f = f,
+        models = models,
+        mod = mod,
+        parcomb = parcomb
+      )
+
+      # Extract results
+      results$rew.acc = append(results$rew.acc, lapply(sim_results, `[[`, "rew.acc"))
+      results$acc.time = append(results$acc.time, lapply(sim_results, `[[`, "acc.time"))
+      results$switches = append(results$switches, lapply(sim_results, `[[`, "switches"))
+      results$switches.time = append(results$switches.time, lapply(sim_results, `[[`, "switches.time"))
+    }
+  }
+  
+  results = lapply(results, function(x) bind_rows(x))
+  saveRDS(results, file = 
+            paste(resultsdir, "numsims_v2.rds", sep = "/"))
+  
+  sink()
+  
+}else{
+  results = readRDS(file = paste(resultsdir, "numsims_v2.rds", sep = "/"))
+}
+
+#### Plot Results For v2 ####
+
+# List to save plots to
+plot.list = list()
+
+# Facet labels
+max.labs = paste("Maximum Yield", sort(unique(results$rew.acc$max)))
+names(max.labs) = sort(unique(results$rew.acc$max))
+
+ratio.labs = paste("Yield Ratio", sort(unique(results$rew.acc$ratio)))
+names(ratio.labs) = sort(unique(results$rew.acc$ratio))
+
+facet.labeller = labeller(ratio=ratio.labs, max=max.labs)
+
+plot.data.c.vsndbr = results$rew.acc %>% 
+  filter(model != "vsnvsr1.fixed") %>% 
+  select(-c(alphaVSD, alphaVSR, alphaVSDR, sigmaVSDR)) %>% # Unused
+  mutate(alphaDBR = map_dbl(alphaDBR, ~ if (is.null(.x)) NA_real_ else .x[1, 1])) %>%  # "Constant" across environments 
+  mutate(alphaS = alphaDBR) %>% 
+  group_by(model, alphaS, ratio, max) %>% # Group by model, social learning weight, ratio, and max
+  reframe(acc.mean.vec = list(acc.mean[which(sim%in% c(1:nsim))])) %>% # Vector of nsim accuracies for each group
+  ungroup() %>%
+  group_by(ratio, max) %>%
+  mutate(
+    acc.mean.vec.vsn = list(acc.mean.vec[model == "vsn2.fixed"][[1]]) # Extract the arl.fixed vector
+  ) %>%
+  ungroup() %>%
+  rowwise() %>%
+  mutate(
+    pairwise = list(as.vector(outer(acc.mean.vec, acc.mean.vec.vsn, "-"))), # Compute pairwise differences
+    acc.delta = mean(pairwise),
+    lower = quantile(pairwise, probs = 0.05),
+    upper = quantile(pairwise, probs = 0.95)
+  ) %>%
+  select(-c(acc.mean.vec, acc.mean.vec.vsn, pairwise)) %>%
+  filter(model != "vsn2.fixed")
+
+plot.data.c.vsnvsr = results$rew.acc %>% 
+  filter(model != "vsndbr2.fixed") %>% 
+  select(-c(alphaVSD, alphaVSR, alphaDBR)) %>% # Unused
+  group_by(model, alphaVSDR, sigmaVSDR, ratio, max) %>% # Group by model, social learning weight, ratio, and max
+  reframe(acc.mean.vec = list(acc.mean[which(sim%in% c(1:nsim))])) %>% # Vector of nsim accuracies for each group
+  ungroup() %>% 
+  group_by(ratio, max) %>%
+  mutate(
+    acc.mean.vec.vsn = list(acc.mean.vec[model == "vsn2.fixed"][[1]]) # Extract the arl.fixed vector
+  ) %>% 
+  ungroup() %>%
+  rowwise() %>%
+  mutate(
+    pairwise = list(as.vector(outer(acc.mean.vec, acc.mean.vec.vsn, "-"))), # Compute pairwise differences
+    acc.delta = mean(pairwise),
+    lower = quantile(pairwise, probs = 0.05),
+    upper = quantile(pairwise, probs = 0.95)
+  ) %>%
+  select(-c(acc.mean.vec, acc.mean.vec.vsn, pairwise)) %>%
+  filter(model != "vsn2.fixed")
+
+# Bind data
+plot.data.c = bind_rows(plot.data.c.vsndbr, plot.data.c.vsnvsr)
+write.csv(plot.data.c, file = paste(resultsdir, "accdiff_v2.csv", sep = "/"))
+
+# Reward-based DB 
+p = plot.data.c %>% filter(model %in% c("vsndbr2.fixed")) %>%
+  ggplot(aes(x=alphaS, fill=as.factor(model), col=as.factor(model))) + 
+  labs(y="Individual Accuracy \n") +
+  scale_y_continuous(breaks=seq(-0.5, 1, by=.1))+
+  scale_x_continuous(name=bquote(paste("\n ", alpha[DBr], " / ", alpha[VSr])), breaks = seq(0, 1, by=.1))+
+  scale_fill_viridis(discrete = T, begin = .1, end = .8, 
+                     name="Model", labels=c("VSnDBr")) +
+  scale_color_viridis(discrete = T, begin = .1, end = .8, 
+                      name="Model", labels=c("VSnDBr")) +
+  geom_hline(yintercept = 0, lty=2)+
+  geom_hline(yintercept = 0.05, lty=2)+
+  geom_ribbon(aes(ymin=lower, ymax=upper), alpha=.5)+
+  geom_line(aes(y=acc.delta), lwd=2) +
+  theme_linedraw(base_size = 11) +
+  facet_grid(ratio ~ max, labeller = facet.labeller)
+p
+#plot.list =  append(plot.list, list(p))
+ggexport(p, width = 2800, height = 1440, 
+         filename = paste(resultsdir, "accdiff_v2_vsndbr.jpeg", sep = "/"))
+
+# Location-based VS Reward-based VS 
+p = plot.data.c %>% filter(model %in% c("vsnvsr1.fixed")) %>%
+  ggplot(aes(x=alphaVSDR, y=sigmaVSDR, fill=acc.delta)) +
+  geom_tile() +
+  scale_fill_viridis(name="Individual Accuracy \n") +
+  theme_linedraw(base_size = 11) +
+  facet_grid(ratio ~ max, labeller = facet.labeller)
+p
+#plot.list =  append(plot.list, list(p))
+ggexport(p, width = 2800, height = 1440, 
+         filename = paste(resultsdir, "accdiff_v2_vsnvsr.jpeg", sep = "/"))
