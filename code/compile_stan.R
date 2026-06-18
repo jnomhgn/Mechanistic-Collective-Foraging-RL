6a8aacb6e0468e18045d6347050d21ae85a7fdd8
jnomhgn <173506318+jnomhgn@users.noreply.github.com>
2026-06-18 15:36:44 +0200

    Track progress when compiling models.

--- code/compile_stan.R
+++ code/compile_stan.R
@@ -4,7 +4,9 @@ setwd(here::here())
 stan_files <- list.files("code", pattern = "\\.stan$", recursive = TRUE, full.names = TRUE)
 
 for (f in stan_files) {
-  message("Compiling: ", f)
+  comp_condition <- ifelse(grepl("alone", f), "alone", ifelse(grepl("nocatches", f), "nocatches", "catches"))
+  comp_model <- gsub("\\.stan$", "", basename(f))
+  message(sprintf("Compiling model %s of %s: %s in %s", which(stan_files == f), length(stan_files), comp_model, comp_condition))
   tryCatch(
     capture.output(cmdstan_model(f, quiet = TRUE), type = "output"),
     error = function(e) message("Failed: ", f, "\n", conditionMessage(e))
